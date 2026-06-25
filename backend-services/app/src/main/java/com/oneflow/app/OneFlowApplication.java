2731f573e644a4c21bec88b05d56a4ba2be37558
lj188 <lj188@users.noreply.github.com>
2026-06-25 15:47:05 +0800

    改造好的java后端，准备测试

--- backend-services/app/src/main/java/com/oneflow/app/OneFlowApplication.java
+++ backend-services/app/src/main/java/com/oneflow/app/OneFlowApplication.java
@@ -1,15 +1,15 @@
-package com.oneflow.api;
+package com.oneflow.app;
 
 import com.oneflow.api.config.OneFlowProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.oneflow.api", "com.oneflow.app"})
 @EnableConfigurationProperties(OneFlowProperties.class)
-public class OneFlowApiApplication {
+public class OneFlowApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OneFlowApiApplication.class, args);
+        SpringApplication.run(OneFlowApplication.class, args);
     }
 }
